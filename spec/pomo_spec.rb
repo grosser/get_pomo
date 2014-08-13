f59bd7098425d8596b388dea2092c727951f4774
Dennis Herr <dennis.herr@experteer.com>
2014-08-13 14:39:34 +0200

    added tests

--- spec/pomo_spec.rb
+++ spec/pomo_spec.rb
@@ -4,4 +4,26 @@ describe GetPomo do
   it "has a VERSION" do
     GetPomo::VERSION.should =~ /^\d+\.\d+\.\d+$/
   end
+
+  it "scans unique translations correctly" do
+    t = GetPomo::PoFile.parse(File.read('spec/files/uniques.po'))
+    t = GetPomo.unique_translations(t)
+    t[0].to_hash.should == {:msgid => "", :msgstr => "", :comment => "# Unique translation test fixture\n#\n#, fuzzy\n"}
+    t[1].to_hash.should == {:msgctxt => "context1", :msgid => "xxx", :msgstr => "yyy", :comment => "#: Reference1\n"}
+    t[2].to_hash.should == {:msgctxt => "context2", :msgid => "xxx", :msgstr => "yyy", :comment => "#: Reference3\n"}
+    t[3].to_hash.should == {:msgctxt => "", :msgid => "xxx", :msgstr => "yyy", :comment => "#: Reference4\n"}
+    t[4].to_hash.should == {:msgid => "xxx", :msgstr => "yyy", :comment => "#: Reference6\n"}
+    t[5].to_hash.should == {:msgid => "xyz", :msgstr => "yyy", :comment => "#: Reference7\n"}
+  end
+
+  it "merges non unique translations correctly" do
+    t = GetPomo::PoFile.parse(File.read('spec/files/uniques.po'))
+    t = GetPomo.unique_translations(t, true)
+    t[0].to_hash.should == {:msgid => "", :msgstr => "", :comment => "# Unique translation test fixture\n#\n#, fuzzy\n"}
+    t[1].to_hash.should == {:msgctxt => "context1", :msgid => "xxx", :msgstr => "yyy", :comment => "#: Reference1\n"}
+    t[2].to_hash.should == {:msgctxt => "context2", :msgid => "xxx", :msgstr => "yyy", :comment => "#: Reference2\n#: Reference3\n"}
+    t[3].to_hash.should == {:msgctxt => "", :msgid => "xxx", :msgstr => "yyy", :comment => "#: Reference4\n"}
+    t[4].to_hash.should == {:msgid => "xxx", :msgstr => "yyy", :comment => "#: Reference5\n#: Reference6\n"}
+    t[5].to_hash.should == {:msgid => "xyz", :msgstr => "yyy", :comment => "#: Reference7\n"}
+  end
 end
